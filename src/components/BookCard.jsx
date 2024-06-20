725e411f9e3adab113844b4bf755bd2fa74dfe7b
Jeronimo Arias <jeronimoarias1212@gmail.com>
2024-06-19 19:56:52 -0500

    show products info

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,11 +1,27 @@
+import { Link } from "react-router-dom"
 
 function BookCard(props) {
-    
-  return (
-    <>
-      
-    </>
-  )
+    const { isbn, titulo, autor, genero, fecha, editorial, imagen } = props
+    return (
+        <>
+            <Link to={`/detail/${isbn}`}>
+                <figure>
+                    <img src={imagen} alt={titulo} />
+                    <figcaption>
+                        <h1>{titulo}</h1>
+                        <div>
+                            <h2>Autor: {autor}</h2>
+                            <h2>Genero: {genero}</h2>
+                        </div>
+                        <div>
+                            <h2>lanzamiento: {fecha}</h2>
+                            <h2>Editorial: {editorial}</h2>
+                        </div>
+                    </figcaption>
+                </figure>
+            </Link>
+        </>
+    )
 }
 
 export default BookCard
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,36 @@
 import { Link } from "react-router-dom"
-
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
+import styles from "./views.module.css"
+import BookCard from "../components/BookCard"
+import books from "../assets/books.json"
 
 function home() {
-  return (
-    <>
-      <Navbar />
-      <Hero firstText="Encuentra tus libros favoritos!" secondText="Registra tu propio libro verificando tu perfil"/>
 
-      <Footer />
-    </>
-  )
+
+    return (
+        <>
+            <Navbar />
+            <Hero firstText="Encuentra tus libros favoritos!" secondText="Registra tu propio libro verificando tu perfil" />
+            <section className={styles["booksSection"]}>
+                {books.map((each) => (
+                    <BookCard
+                        key={each.isbn}
+                        isbn={each.isbn}
+                        titulo={each.titulo}
+                        autor={each.autor}
+                        genero={each.genero}
+                        fecha={each.fecha}
+                        editorial={each.editorial}
+                        imagen={each.imagen}
+                    />
+                ))}
+            </section>
+            <Footer />
+        </>
+    )
 }
 
 export default home
